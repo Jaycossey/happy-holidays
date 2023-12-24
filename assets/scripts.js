0485a2aea8352efc69d09c4bb7f07ba7b54918ef
jaycossey <jaycossey@live.com>
2023-12-24 15:52:56 +0000

    finished project

--- assets/scripts.js
+++ assets/scripts.js
@@ -10,6 +10,7 @@ const unwrapGift = (element) => {
     ROOT_DIV.removeChild(element);
 
     startSnowing();
+    setCardMessage();
 }
 
 const handleSubmit = () => {
@@ -49,11 +50,49 @@ class Snow {
         this.size = size;
         this.position = position;
         this.speed = speed;
+        this.element = document.createElement('div');
+        this.element.style.width = this.size + "px";
+        this.element.style.height = this.size + "px";
+        this.element.classList = "snowFlake";
+        this.element.style.left = `${getRandom(100)}vw`;
     }
+}
+
+const getRandom = (options) => {
+    return Math.floor(Math.random() * options);
 }
 
 const startSnowing = () => {
     const snowArray = [];
 
-    
+    // call this for each snowflake object
+    let randomNumber = getRandom(10) + 10;
+
+    for (let i = 0; i < randomNumber; i++) {
+        let snowFlake = new Snow(getRandom(50), getRandom(100), getRandom(20));
+        snowArray.push(snowFlake);
+        setTimeout(() => {
+            ROOT_DIV.append(snowArray[i].element);
+        }, getRandom(3000) * i);
+    }
+
 }
+
+// startSnowing();
+
+// CARD GREETINGS ----------------------------
+const setCardMessage = () => {
+    const bottomDiv = document.createElement('div');
+    bottomDiv.className = "fadeUpBg";
+
+    bottomDiv.innerText = `Happy Holidays ${userName}!\nWishing you and yours all the best for the new year!\nIan and Family`;
+
+    const giftDiv = document.createElement('div');
+    giftDiv.classList = "gift";
+
+    bottomDiv.append(giftDiv);
+
+    ROOT_DIV.append(bottomDiv);
+}
+
+// setCardMessage();
